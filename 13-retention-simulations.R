17d1f04054a7ae6ffeb6eec74f210a1a9fd8a08a
Morten Wang <nettrom@gmail.com>
2020-05-04 16:38:19 -0700

    Add retention simulation function
    Similar function as we used for activation, just for retention.

--- 13-retention-simulations.R
+++ 13-retention-simulations.R
@@ -3,12 +3,54 @@
 ## Assumes that 06-stat-load-libraries.R as well as the functions
 ## from 07-activation-simulations.R are all loaded into memory.
 
+run_retention_simulations = function(act_counts, coeffs) {
+  ## Run simulations of user retention with the given activation counts
+  ## and coefficients. Returns a flattened data.table with results for
+  ## 10%, 5%, and 2% effect size.
+
+  ## We calculate retention power on a monthly basis from 1 to 6 months,
+  ## in half-month increments
+  multipliers <- seq(1, 6, by = 0.5)
+  # So the output is also named, with the estimated power for each multiplier:
+  multipliers <- setNames(multipliers, multipliers)
+  # Then use the ~ syntax to define an anonymous function applied to each multiplier:
+  retention_powers = list()
+  retention_powers[['10%']] <- purrr::map_dbl(
+    multipliers,
+    ~ estimate_power(floor(act_counts * .x), coeffs)
+  )
+
+  ## What if the effect is 5%?
+  coeffs[['beta']] = 0.2
+  coeffs[['sigma_beta']] = 0.1
+
+  retention_powers[['5%']] <- purrr::map_dbl(
+    multipliers,
+    ~ estimate_power(floor(activation_counts * .x), coeffs)
+  )
+
+  ## What if the effect is 2%?
+  coeffs[['beta']] = 0.08
+  coeffs[['sigma_beta']] = 0.04
+
+  retention_powers[['2%']] <- purrr::map_dbl(
+    multipliers,
+    ~ estimate_power(floor(activation_counts * .x), coeffs)
+  )
+  
+  ## Flatten the list into a data.frame
+  x = data.table::rbindlist(lapply(retention_powers, as.data.frame.list))
+  x[, `Effect size` := ordered(c("10%", "5%", "2%"), c("10%", "5%", "2%"))]
+  return(x)
+}
+
+
 ## 1: Load in the parameters
 load('simulation_parameters/first4_retention_params.txt', verbose = TRUE)
 ## 2: Run simulations
-first4_retention_results = run_power_simulations(act_counts_first4, ret_coeff_first4)
+## first4_retention_results = run_power_simulations(act_counts_first4, ret_coeff_first4)
 
 ## 3: save output as a TSV
-write.table(first4_retention_results, file = 'datasets/first4-retention-results.tsv',
-            sep = '\t', quote = FALSE, row.names = FALSE)
+##write.table(first4_retention_results, file = 'datasets/first4-retention-results.tsv',
+##            sep = '\t', quote = FALSE, row.names = FALSE)
 
